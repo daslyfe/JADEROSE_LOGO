73662cf9076e7684b5476572f24f70f6b8e59ad3
Jade Rose <moosella44@gmail.com>
2020-08-17 21:15:20 -0400

    new repos

---
+++ build/precache-manifest.62b89a60b3840caf5c3af4ae7d923341.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120efda980be692c712a42dca234f9e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3b6f8585dfb61b83b0",
+    "url": "/static/css/main.2bfe7d62.chunk.css"
+  },
+  {
+    "revision": "27a2f52c5b3ac88ca3a1",
+    "url": "/static/js/2.53ebb0f5.chunk.js"
+  },
+  {
+    "revision": "806a98f318263a7af6eb94d73465b791",
+    "url": "/static/js/2.53ebb0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3b6f8585dfb61b83b0",
+    "url": "/static/js/main.f79095e9.chunk.js"
+  },
+  {
+    "revision": "ae05e168283b2039183f",
+    "url": "/static/js/runtime-main.4d0b2e8b.js"
+  }
+]);
